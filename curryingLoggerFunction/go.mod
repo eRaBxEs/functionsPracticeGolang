d3e17286284db466af0a8842258487046afc715d
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-08-28 20:48:51 +0100

    use closure/currying for logger function

---
+++ curryingLoggerFunction/go.mod
@@ -0,0 +1,3 @@
+module currying-logger-function
+
+go 1.23.4
---
+++ curryingLoggerFunction/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func getLogger(formatter func(string, string) string) func(string, string) {
+	return func(first, second string) {
+		result := formatter(first, second)
+		fmt.Println(result)
+	}
+}
+
+func test(first string, errors []error, formatter func(string, string) string) {
+	defer fmt.Println("====================================")
+	logger := getLogger(formatter)
+	fmt.Println("Logs:")
+	for _, err := range errors {
+		logger(first, err.Error())
+	}
+
+}
+
+func colonDelimit(first, second string) string {
+	return first + ":" + second
+}
+
+func commaDelimit(first, second string) string {
+	return first + "," + second
+}
+
+func main() {
+
+	dbErrors := []error{
+		errors.New("out of memory"),
+		errors.New("cpu is pegged"),
+		errors.New("networking issue"),
+		errors.New("invalid syntax"),
+	}
+
+	test("Error on database server", dbErrors, colonDelimit)
+
+	mailErrors := []error{
+		errors.New("email too large"),
+		errors.New("non alphanumeric symbols found"),
+	}
+
+	test("Error on mail server", mailErrors, commaDelimit)
+
+}
